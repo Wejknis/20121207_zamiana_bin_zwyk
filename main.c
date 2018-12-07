bfa8ee717ca6bac333a63d9acb42353cdf2b8245
Wejknis <weja077@gmail.com>
2018-12-07 15:18:02 +0100

    zamiana ciągu binarnego na zwykły początek koniec

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int tablica[200];
+    char wartosc = 0;
+    int index = 0;
+
+    printf("Program liczy wartość dziesiętną\n "
+           "dla ciągu binarnego\n");
+    printf("Podaj ciąg binarny (0/1): ");
+
+    do{
+        scanf(" %c", &wartosc);
+        if(wartosc == '0'  || wartosc == '1'){
+            tablica[index] = wartosc - '0';
+            index++;
+        }
+    }while(wartosc == '0'  || wartosc == '1');
+
+    printf("Długość ciągu %d", index);
+
     return 0;
 }
